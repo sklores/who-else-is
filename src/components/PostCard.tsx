fe3f1000e1049db16e37f7fd27ad49eeeafd5885
sklores <sklores@gmail.com>
2026-01-29 21:52:59 -0500

    Restyle Reddit view

---
+++ src/components/PostCard.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+type Post = {
+  id: string
+  name: string
+  text: string
+  time: string
+  iamCount: number
+}
+
+type PostCardProps = {
+  post: Post
+}
+
+function PostCard({ post }: PostCardProps) {
+  const [iamCount, setIamCount] = useState(post.iamCount)
+  const [hasAcknowledged, setHasAcknowledged] = useState(false)
+
+  const handleClick = () => {
+    if (hasAcknowledged) {
+      return
+    }
+
+    setIamCount((count) => count + 1)
+    setHasAcknowledged(true)
+  }
+
+  return (
+    <article>
+      <h2>{post.name}</h2>
+      <p>{post.text}</p>
+      <p className="post-meta">
+        <span>{post.time}</span>
+        <span>I also be {iamCount}</span>
+      </p>
+      <button
+        type="button"
+        onClick={handleClick}
+        disabled={hasAcknowledged}
+      >
+        I am
+      </button>
+    </article>
+  )
+}
+
+export default PostCard
+export type { Post }
